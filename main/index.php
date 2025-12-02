2bf84c433c08e01c899f3c2aa2ba1851c5e3a425
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-12-02 18:47:07 +0100

    mod(index.php): Added new changes implementation

--- main/index.php
+++ main/index.php
@@ -3,20 +3,26 @@
 declare(strict_types=1);
 require __DIR__ . "/../vendor/autoload.php";
 
-use SmartGoblin\Components\Core\Server;
+use SmartGoblin\Server;
+
 use SmartGoblin\Components\Core\Config;
 use SmartGoblin\Components\Core\Template;
 use SmartGoblin\Components\Routing\Endpoint;
 use SmartGoblin\Components\Routing\Router;
 
-// Main Configuration. sitePath SHOULD ALWAYS BE __DIR__ unless you know what you are doing. siteName is NOT the subdomain.
-$config = Config::new(__DIR__, "main", false);
+use SmartGoblin\Workers\Bee;
+
+// Configuration loader. sitePath SHOULD ALWAYS BE __DIR__ unless you know what you are doing.
+Server::preload(__DIR__);
+
+// Main Configuration. siteName is not the subdomain.
+$config = Config::new("main", false);
 
 // Which hosts are allowed to request the API.
-$config->configureAllowedHosts(["127.0.0.1", "localhost"]);
+$config->configureAllowedHosts([Bee::env("SITE_ADDRESS", "localhost")]);
 
 // Cookie Session Configuration. Domain can be set to .domain.com to accept all subdomains inside your domain.
-$config->configureAuthorization("YOURSESSIONNAME", 7, "localhost");
+$config->configureAuthorization("YOURSESSIONNAME", 7, Bee::env("SITE_ADDRESS", "localhost"));
 
 // Where to redirect if route not found.
 $config->configureNotFoundRedirects("/");
@@ -25,7 +31,7 @@ $config->configureNotFoundRedirects("/");
 $config->configureUnauthorizedRedirects("/", "");
 
 // Default template for all views.
-$template = Template::new("Smart Site", "0.0.1", "en");
+$template = Template::new("Smart Site", Bee::env("VERSION", "0.0.1"), "en");
 
 // View Router configuration. You can append a template to each view.
 $viewRouter = Router::endpoints([
@@ -42,13 +48,13 @@ $apiRouter = Router::endpoints([
     Endpoint::api(false, "GET","/ping", "Pong.php")
 ]);
 
-// If router gets to big, use Router::files() instead.
+// If router gets to big, use Router::files() instead and refer to config/router files.
 // $apiRouter = Router::files(["/v1" => "main.php", "/v2" => "new_version/main.php"]);
 
 /* ------------------------------------------------------- */
 
 // Server initialization.
-$server = Server::new();
+$server = Server::createInstance();
 
 // Server configuration.
 $server->configure($config, $template, $viewRouter, $apiRouter);

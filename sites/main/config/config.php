7da6ebf691a64a0569a2d717b7205c5d4dff2ddd
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-12-07 12:32:20 +0100

    ntsh(): adapt to last changes

--- sites/main/config/config.php
+++ sites/main/config/config.php
@@ -7,9 +7,6 @@ use FastRaven\Workers\Bee;
 // Main Configuration. siteName is not the subdomain.
 $config = Config::new("main", false);
 
-// Which hosts are allowed to request the API.
-$config->configureAllowedHosts([Bee::env("SITE_ADDRESS", "localhost")]);
-
 // Cookie Session Configuration. Domain can be set to .domain.com to accept all subdomains inside your domain.
 $config->configureAuthorization("YOURSESSIONNAME", 7, Bee::env("SITE_ADDRESS", "localhost"));
 
@@ -18,6 +15,9 @@ $config->configureNotFoundRedirects("/");
 
 // Where to redirect if not authorized. Leave subdomain empty to use the main domain. DO NOT USE a restricted site.
 $config->configureUnauthorizedRedirects("/", "");
+
+// Define whether to register logs or restrict what data to register.
+$config->configurePrivacy(true, true);
 
 return $config;
 

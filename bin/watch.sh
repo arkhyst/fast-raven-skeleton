6563f1519dd3a39e1f493a8806989112571d8f62
R3DG0BL1N <58450310+R3DG0BL1N@users.noreply.github.com>
2025-12-09 11:25:01 +0100

    fix(watch.sh): Sites folder not accounted for watch.sh

--- bin/watch.sh
+++ bin/watch.sh
@@ -16,8 +16,8 @@ projects=("main")
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
 
 for proj in "${projects[@]}"; do
-    sass --watch "$proj/src/resources":"$proj/public/resources" --style=compressed &
-    chokidar "$proj/src/resources/*.js" -c "javascript-obfuscator $proj/src/resources --output $proj/public/resources --compact true --self-defending true > /dev/null 2>&1" &
+    sass --watch "sites/$proj/src/resources":"sites/$proj/public/resources" --style=compressed &
+    chokidar "sites/$proj/src/resources/*.js" -c "javascript-obfuscator sites/$proj/src/resources --output sites/$proj/public/resources --compact true --self-defending true > /dev/null 2>&1" &
 done
 
 wait

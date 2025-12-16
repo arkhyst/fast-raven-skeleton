ac8be431e1eac002856ac9c07f9e80ceab9e599d
Arkhyst <rodriguezeloy99@gmail.com>
2025-12-16 23:50:50 +0100

    refactor(): New skeleton structure, more organized and scalable.

--- sites/main/config/template.php
+++ sites/main/config/template.php
@@ -5,7 +5,7 @@ use FastRaven\Components\Core\Template;
 use FastRaven\Workers\Bee;
 
 // Default template for all views.
-$template = Template::new("FastRaven Site", Bee::env("VERSION", "0.0.1"), "en");
+$template = Template::new("Fast Raven Site", Bee::env("VERSION", "0.0.1"), "en");
 
 return $template;
 

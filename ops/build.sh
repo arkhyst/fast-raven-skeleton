70db6f32d7f3ae93627c79beda9db0c85ddb1918
Arkhyst <rodriguezeloy99@gmail.com>
2026-01-27 18:10:22 +0100

    mod: Template::addData() now accepts string parameters instead of Pair objects.

--- ops/build.sh
+++ ops/build.sh
@@ -435,7 +435,7 @@ use FastRaven\Components\Data\Item;
 
 return function(Request $request, Template $baseTemplate): Template {
     $page = Template::new("ping.php", $baseTemplate->getTitle()." - Ping test");
-    $page->addData(Item::new("test", "This is a test"));
+    $page->addData("test", "This is a test");
     return $page;
 };
 EOF
--- sites/main/src/views/Ping.php
+++ sites/main/src/views/Ping.php
@@ -2,10 +2,9 @@
 
 use FastRaven\Components\Core\Template;
 use FastRaven\Components\Http\Request;
-use FastRaven\Components\Data\Item;
 
 return function(Request $request, Template $baseTemplate): Template {
     $page = Template::new("ping.php", $baseTemplate->getTitle()." - Ping test");
-    $page->addData(Item::new("test", "This is a test"));
+    $page->addData("test", "This is a test");
     return $page;
 };
